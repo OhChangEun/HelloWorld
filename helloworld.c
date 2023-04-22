250f49c8483a5efbda2146ee30da703538e92d6c
OhChangEun <dwc07109@gmail.com>
2023-04-22 20:08:25 +0900

    branch2 작업 실행

--- helloworld.c
+++ helloworld.c
@@ -2,9 +2,7 @@
 
 int main(int argc, char* argv[]){
 
-	printf("hello world\n");
-	printf("b1\n");
-	printf("b2\n");	
-	
+	printf("Merge Conflict 구현해보기\n");
+	printf("Branch 2 작업 실행\n");	
 	return 0;
 }
